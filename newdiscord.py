846dc378152d2c8c6dbe24ce04be806cd988a53c
sAnAnAmE1 <54670712+sAnAnAmE1@users.noreply.github.com>
2019-08-30 20:39:19 +0900

    Update newdiscord.py

--- newdiscord.py
+++ newdiscord.py
@@ -99,5 +99,5 @@ async def on_message(message):
         await author.remove_roles(role)
 
          
-access_token = os.environ["BOT TOKEN"]            
+access_token = os.environ["BOT_TOKEN"]            
 client.run(access_token)
